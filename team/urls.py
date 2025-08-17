919cb64e4461f6ae8ee9afa465dc7ad36ea5f334
HyperOrange <12201966@inha.edu>
2025-08-17 13:29:45 +0900

    8_17 1차 수정
    팀원 추가 버튼 아래 밑줄 생기는 오류 해결
    팀원 리스트 수정 기능 추가

--- team/urls.py
+++ team/urls.py
@@ -5,4 +5,5 @@ app_name = 'team'
 
 urlpatterns = [
     path('<uuid:team_id>/add-member/', views.add_member, name='add_member'),
+    path('<str:team_id>/edit_member/<int:member_id>/', views.edit_member, name='edit_member'),
 ]
--- team/views.py
+++ team/views.py
@@ -22,5 +22,30 @@ def add_member(request, team_id):
     
     context = {
         'team_id': team.team_id,
+        'is_edit': False,
+    }
+    return render(request, 'team/profile_settings.html', context)
+
+def edit_member(request, team_id, member_id):
+    team = get_object_or_404(Team, team_id=team_id)
+    member = get_object_or_404(TeamMember, id=member_id, team=team)
+    
+    if request.method == 'POST':
+        nickname = request.POST.get('nickname')
+        profile_image = request.POST.get('profile_image')
+        tags = request.POST.get('tags')
+
+        if nickname:
+            member.nickname = nickname
+            member.profile_image = profile_image
+            member.tags = tags
+            member.save()
+            # 수정 후 메인 페이지로 리다이렉트
+            return redirect(reverse('core:main_page', kwargs={'team_id': team.team_id}))
+    
+    context = {
+        'team_id': team.team_id,
+        'member': member,
+        'is_edit': True,
     }
     return render(request, 'team/profile_settings.html', context)
