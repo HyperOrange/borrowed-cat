d58f6389d08404d4ce3c0f2016178e870f0244b0
HyperOrange <12201966@inha.edu>
2025-08-19 22:25:51 +0900

    투두리스트 추가 전

--- myteam_planner/urls.py
+++ myteam_planner/urls.py
@@ -1,3 +1,4 @@
+# myteam_planner/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,9 +7,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('todo/', include('todo_list.urls')),
-    path('team/', include('team.urls')), # team 앱의 URL을 추가합니다.
+    # 'todo_list' 앱을 삭제했으므로 해당 URL 패턴도 제거합니다.
+    # path('todo/', include('todo_list.urls')),
+    path('team/', include('team.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todo_list/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'todo_list'
-
-urlpatterns = [
-    path('<uuid:team_id>/', views.list, name='list'),
-    path('<uuid:team_id>/new/', views.new, name='new'),
-]
