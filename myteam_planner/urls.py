f9af220db2d25af7eb5048b10c787f913b49c189
chocoboom99 <maple7868@gmail.com>
2025-08-20 23:20:12 +0900

    수정

--- myteam_planner/urls.py
+++ myteam_planner/urls.py
@@ -1,4 +1,4 @@
-# MyTeamPlanner/myteam_planner/myteam_planner/urls.py
+# myteam_planner/urls.py
 
 from django.contrib import admin
 from django.urls import path, include
@@ -7,7 +7,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # core 앱의 URL을 포함할 때 namespace를 'core'로 지정합니다.
-    path('', include(('core.urls', 'core'), namespace='core')), # <-- 이 줄을 변경
+    path('', include(('core.urls', 'core'), namespace='core')),
     path('team/', include(('team.urls', 'team'), namespace='team')),
+    # 역할 분배 앱의 URL을 기존 방식에 맞춰 추가합니다.
+    path('role/', include(('role_distribution.urls', 'role_distribution'), namespace='role_distribution')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
